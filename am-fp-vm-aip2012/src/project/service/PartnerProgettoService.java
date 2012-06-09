cabdd90c3e6763c51b474018a1828c0e0ce03785
MacAury88 <aury88@libero.it>
2012-06-09 18:00:52 +0200

    Implementate alcune funzioni per la gestione del collegamento tra Partner e Progetto. (sperem =) )

--- am-fp-vm-aip2012/src/project/service/PartnerProgettoService.java
+++ am-fp-vm-aip2012/src/project/service/PartnerProgettoService.java
@@ -1,8 +1,10 @@
 package project.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slim3.datastore.Datastore;
+import org.slim3.datastore.ModelQuery;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Transaction;
@@ -42,8 +44,82 @@ public class PartnerProgettoService {
         return collegamento;
     }
     
+    /**
+     * Restituisce una lista di partners appartententi al
+     * progetto dato.
+     * @param progetto la Key del progetto
+     * @return Una lista di Partners (eventualmente vuota)
+     * @see PartnerProgettoService#getPartnerByProgetto(Progetto)
+     */
     public List<Partner> getPartnerByProgetto(Key progetto){
-        //TODO IMPLEMENT!!
-        return null;
+       Progetto prg = Datastore.get(mProgetto, progetto);
+       return this.getPartnerByProgetto(prg);
+    }
+    
+    /**
+     * Restituisce una lista di partners appartententi
+     * al progetto dato.
+     * @see PartnerProgettoService#getPartnerByProgetto(Key)
+     * @param progetto Il Progetto associato
+     * @return Una lista di Partner
+     */
+    public List<Partner> getPartnerByProgetto(Progetto progetto){
+        List<Partner> partners = new ArrayList<Partner>();
+        for(PartnerProgetto pp: progetto.getPartnerProgettoListRef().getModelList())
+            partners.add(pp.getPartnerRef().getModel());
+        return partners;
+    }
+    
+    /**
+     * Restituisce una lista di partners appartenenti
+     * al progetto dato.
+     * @param partner Il Partner associato
+     * @return una lista di Progetti
+     */
+    public List<Progetto> getProgettoByPartner(Partner partner){
+        List<Progetto> progetti = new ArrayList<Progetto>();
+        for(PartnerProgetto pp: partner.getPartnerProgettoListRef().getModelList())
+            progetti.add(pp.getProgettoRef().getModel());
+        return progetti;
+    }
+    
+    /**
+     * Elimina solo il collegamento tra partner e progetto le cui chiavi
+     * vengono fornite come parametri.
+     * Se l'operazione ha successo e il collegamento viene eliminato,
+     * il metodo ritorna true, false altrimenti.
+     * @param partnerKey La Key del Partner
+     * @param progettoKey La Key del Progetto
+     * @return true se il collegamento viene eliminato, false se non viene trovato alcun
+     * collegamento.
+     */
+    public boolean eliminaCollegamento(Key partnerKey, Key progettoKey){
+        Partner partner = Datastore.get(mPartner, partnerKey);
+        Progetto progetto = Datastore.get(mProgetto, progettoKey);
+        for(PartnerProgetto pp: partner.getPartnerProgettoListRef().getModelList())
+            if(pp.getProgettoRef().getModel().equals(progetto)){
+                Datastore.delete(pp.getKey());
+                return true;
+            }
+        return false;
+    }
+    
+    /**
+     * Elimina sia il collegamento tra Partner e Progetto, sia le due entità
+     * (Partner e Progetto).
+     * Restituisce true se l'operazione ha successo, false se non viene
+     * trovato alcun collegamento.
+     * Utilizza il metodo {@link PartnerProgettoService#eliminaCollegamento(Key, Key)}
+     * @see PartnerProgettoService#eliminaCollegamento(Key, Key)
+     * @param partnerKey La Key del Partner da eliminare insieme al collegamento
+     * @param progettoKey La Key del Progetto da eliminare insieme al collegamento
+     * @return true se l'operazione ha successo, false altrimenti.
+     */
+    public boolean eliminaTutto(Key partnerKey, Key progettoKey){
+        if(eliminaCollegamento(partnerKey, progettoKey)){
+            Datastore.delete(progettoKey,partnerKey);
+            return true;
+        }
+        return false;
     }
 }
